803bdc7a0610472a6bde96974c0cd8c9e4fb82ad
Kirill <abqwerty390@gmail.com>
2023-10-11 08:42:16 +0300

    added logic for spawning start and finish platforms

--- Assets/Scripts/TowerBuilder.cs
+++ Assets/Scripts/TowerBuilder.cs
@@ -7,6 +7,11 @@ public class TowerBuilder : MonoBehaviour
     [SerializeField] private SpawnPlatform _spawnPlatform;
     [SerializeField] private Platform[] _platforms;
     [SerializeField] private FinishPlatform _finishPlatform;
+    [SerializeField] private float _additionalScale = 3f;
+
+    private float _additionalScaleForStartAndFinishPlatform = 0.5f;
+
+    private float BeamScaleY => _levelCount / 2f + _additionalScaleForStartAndFinishPlatform + _additionalScale / 2f;
 
     private void Start()
     {
@@ -16,15 +21,19 @@ public class TowerBuilder : MonoBehaviour
     private void Build()
     {
         GameObject beam = Instantiate(_beam, transform.position, Quaternion.identity, transform);
-        beam.transform.localScale = new Vector3(1f, _levelCount / 2f, 1f);
+        beam.transform.localScale = new Vector3(1f, BeamScaleY, 1f);
 
         Vector3 spawnPosition = beam.transform.position;
-        spawnPosition.y += beam.transform.localScale.y;
+        spawnPosition.y += beam.transform.localScale.y - _additionalScale;
+
+        SpawnPlatform(_spawnPlatform, ref spawnPosition, beam.transform);
 
         for (int i = 0; i < _levelCount; i++)
         {
             SpawnPlatform(_platforms[Random.Range(0, _platforms.Length)], ref spawnPosition, beam.transform);
         }
+
+        SpawnPlatform(_finishPlatform, ref spawnPosition, beam.transform);
     }
 
     private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
