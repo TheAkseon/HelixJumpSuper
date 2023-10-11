1f60ea259be30554678dc7f7f5d648d269a9a717
Kirill <abqwerty390@gmail.com>
2023-10-11 09:38:04 +0300

    added ball jump

--- Assets/Scripts/Ball/BallJumper.cs
+++ Assets/Scripts/Ball/BallJumper.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class BallJumper : MonoBehaviour
 {
+    [SerializeField] private float _jumpForce;
+
     private Rigidbody _rigidbody;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PlatformSegment platformSegment))
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
     }
 }
