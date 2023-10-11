9df4c9b300d8c35156bc92369c4613f591683406
Kirill <abqwerty390@gmail.com>
2023-10-11 08:31:49 +0300

    added logic for spawning regular platforms

--- Assets/Scripts/TowerBuilder.cs
+++ Assets/Scripts/TowerBuilder.cs
@@ -17,5 +17,19 @@ public class TowerBuilder : MonoBehaviour
     {
         GameObject beam = Instantiate(_beam, transform.position, Quaternion.identity, transform);
         beam.transform.localScale = new Vector3(1f, _levelCount / 2f, 1f);
+
+        Vector3 spawnPosition = beam.transform.position;
+        spawnPosition.y += beam.transform.localScale.y;
+
+        for (int i = 0; i < _levelCount; i++)
+        {
+            SpawnPlatform(_platforms[Random.Range(0, _platforms.Length)], ref spawnPosition, beam.transform);
+        }
+    }
+
+    private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
+    {
+        Instantiate(platform, spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0), parent);
+        spawnPosition.y -= 1;
     }
 }
